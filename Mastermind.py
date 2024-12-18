daae93b706106c9fc76e24ee954f59447950558e
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2024-12-18 13:41:25 +0100

    Update Mastermind.py
    Etape 2

--- Mastermind.py
+++ Mastermind.py
@@ -13,5 +13,17 @@ def secret_combination():
 secret_test = secret_combination()
 print(f"Combinaison secrète générée (test) : {secret_test}")
 
+#Etape 2 : Récupération de la proposition du joueur
+def get_proposition():
+    while True:
+        choice = input(f"Saisissez votre choix ({combination_len} chiffres parmi {', '.join(map(str, combination_value))}): ")
+        # Vérification de la longueur et des valeurs
+        if len(choice) == combination_len and all(char.isdigit() and int(char) in combination_value for char in choice):
+            return [int(char) for char in choice]  # Conversion en liste d'entiers
+        else:
+            print(f"Saisie invalide. Vérifiez que votre choix est composé de {combination_len} chiffres et utilise seulement ces valeurs : {', '.join(map(str, combination_value))}.")
 
+# Test de la fonction get_proposition
+player_choice = get_proposition()
+print(f"Choix du joueur : {player_choice}")
 
