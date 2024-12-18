0639669c7bc90864115ed4afc0582308d71c33b7
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2024-12-18 13:47:07 +0100

    Update Mastermind.py
    Etape 4

--- Mastermind.py
+++ Mastermind.py
@@ -1,19 +1,18 @@
 import random  # Pour la sélection de nombres aléatoires
 
-# Définition des variables
+#Etape 1: Définition des variables
 combination_len = 4
 maximum_try = 10
 combination_value = [1, 2, 3, 4, 5, 6]
 
-#Etape 1: Génération de la combinaison secrète
+#Etape 2: Génération de la combinaison secrète
 def secret_combination():
     return [random.choice(combination_value) for _ in range(combination_len)]
-
 # Test de la fonction secret_combination
 secret_test = secret_combination()
 print(f"Combinaison secrète générée (test) : {secret_test}")
 
-#Etape 2 : Récupération de la proposition du joueur
+#Etape 3: Récupération de la proposition du joueur
 def get_proposition():
     while True:
         choice = input(f"Saisissez votre choix ({combination_len} chiffres parmi {', '.join(map(str, combination_value))}): ")
@@ -22,8 +21,28 @@ def get_proposition():
             return [int(char) for char in choice]  # Conversion en liste d'entiers
         else:
             print(f"Saisie invalide. Vérifiez que votre choix est composé de {combination_len} chiffres et utilise seulement ces valeurs : {', '.join(map(str, combination_value))}.")
-
 # Test de la fonction get_proposition
 player_choice = get_proposition()
 print(f"Choix du joueur : {player_choice}")
+
+#Etape 4: Vérification de la proposition du joueur
+def verify_proposition(secret_test, choice):
+    well_placed = sum(s == c for s, c in zip(secret_test, choice))
+    # Comptage des chiffres correctement placés et mal placés
+    secret_test_counts = {number: secret_test.count(number) for number in combination_value}
+    choice_counts = {number: choice.count(number) for number in combination_value}
+    correct_numbers = sum(min(secret_test_counts[number], choice_counts[number]) for number in combination_value)
+    misplaced = correct_numbers - well_placed
+    return well_placed, misplaced
+# Test de la fonction verify_proposition
+well_placed, misplaced = verify_proposition(secret_test, player_choice)
+print(f"Nombre de Chiffres bien placés : {well_placed}, Nombre de Chiffres mal placés : {misplaced}")
+
+
+
+
+
+
+
+
 
